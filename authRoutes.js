50243720dd86c0f8584fcda18d4ea1725cf76e68
samuckinha <samuel.schamne.nascimento@escola.pr.gov.br>
2026-08-18 10:48:55 -0300

    Deleted authRoutes.js

--- authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Rota POST que o formulário de login chamará
-router.post('/login', authController.login);
-
-module.exports = router;
